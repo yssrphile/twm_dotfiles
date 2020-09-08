078d848d0c6dd7bf901cea5b343bcee8208b094e
yssrphile <57321521+yssrphile@users.noreply.github.com>
2020-09-07 20:09:13 -0500

    Add files via upload
    added dmenu folder (source file + patches)

---
+++ dmenu/config.h
@@ -0,0 +1,32 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=9"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f6f6c9", "#021b21" },
+	[SchemeSel] = { "#032c36", "#a2d9b1" },
+	[SchemeSelHighlight] = { "#8a7a63", "#beb090" },
+	[SchemeNormHighlight] = { "#9ed9d8", "#44b5b1" },
+	[SchemeOut] = { "#065f73", "#032c36" },
+	[SchemeMid] = { "#ff99a1", "#ff5879" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 5;
