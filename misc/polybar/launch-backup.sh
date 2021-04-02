4e82475c35ccf94276075370d4612f6812cbb4eb
yssrphile <57321521+yssrphile@users.noreply.github.com>
2021-04-02 15:09:59 -0500

    modified config
    made new backups and modified the polybar shape and colors

--- misc/polybar/launch-backup.sh
+++ misc/polybar/launch-backup.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar01 >>/tmp/polybar1.log 2>&1 & disown
-polybar bar02 >>/tmp/polybar2.log 2>&1 & disown
+polybar example >>/tmp/polybar1.log 2>&1 & disown
+##polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."
