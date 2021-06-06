8d364451b44370953593654e5e945cdfac7ef1af
yssrphile <57321521+yssrphile@users.noreply.github.com>
2021-06-06 12:51:10 -0500

    added customLock file

---
+++ local/bin/customLock.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+i3lock -f -b -B sigma --indicator --ringcolor=7b8748 --linecolor=1f1f1f --ringwrongcolor=d17b49 --insidewrongcolor=d17b4980 --ringvercolor=535c5c --insidecolor=6d715e80 --insidevercolor=444b4b80 --keyhlcolor=647035 bshlcolor=402e2e --verifcolor=c0b18b --wrongcolor=c0b18b && sleep 1
+
+## this is a custom script that uses i3lock to lock the screen
+## it is using the "sweetlove" colorscheme from terminal.sexy
