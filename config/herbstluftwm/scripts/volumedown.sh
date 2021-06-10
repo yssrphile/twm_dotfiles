108145e251e503a375aab4a016c5373805f0f1e0
yssrphile <57321521+yssrphile@users.noreply.github.com>
2021-06-09 19:51:03 -0500

    add herbstluftwm files
    added herbstluftwm autostart, panel (which is non-functional), and [functional] volume scripts

---
+++ config/herbstluftwm/scripts/volumedown.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#amixer set Master 5-
+#a=`amixer get Master |grep % |awk '{print $5}'|sed -e 's/\[//' -e 's/\]//'`
+#dunstify "Volume down(${a:0:3})"
+
+VOLUME='-1%'
+for SINK in `pacmd list-sinks | grep 'index:' | cut -b12-`
+do
+  pactl set-sink-volume $SINK $VOLUME
+done
